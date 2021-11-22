629058f228de1a6d8690911abe3069f3189d9794
gckopper <41166074+gckopper@users.noreply.github.com>
2021-11-22 01:03:15 -0300

    First working version

--- cloudflare/cloudflare.go
+++ cloudflare/cloudflare.go
@@ -7,9 +7,13 @@ import (
 	"strings"
 )
 
-func UpdateAAAA(version int, zoneid string, recordid string, email string, authkey string, body string) int {
+func UpdateAAAA(version int, zoneid string, recordid string, email string, authkey string, recordType string, domain string, ipAddr string) int {
 	client := &http.Client{}
-	req, err := http.NewRequest("PUT", fmt.Sprintf("https://api.cloudflare.com/client/v%d/zones/%s/dns_records/%s", version, zoneid, recordid), strings.NewReader(body))
+	url := fmt.Sprintf("https://api.cloudflare.com/client/v%d/zones/%s/dns_records/%s", version, zoneid, recordid)
+	body := fmt.Sprintf("{\"type\":\"%s\",\"name\":\"%s\",\"content\":\"%s\",\"ttl\":1,\"proxied\":false}", recordType, domain, ipAddr)
+	//fmt.Println(url)
+	//fmt.Println(body)
+	req, err := http.NewRequest("PUT", url, strings.NewReader(body))
 	req.Header.Set("X-Auth-Email", email)
 	req.Header.Set("X-Auth-Key", authkey)
 	req.Header.Set("Content-Type", "application/json")
--- main.go
+++ main.go
@@ -2,10 +2,28 @@ package main
 
 import (
 	"fmt"
+	"os"
 
+	"github.com/gckopper/glowing-giggle/cloudflare"
 	"github.com/gckopper/glowing-giggle/glowing"
 )
 
 func main() {
-	fmt.Println(glowing.GetIP())
+	//fmt.Println(glowing.GetIP())
+	file, err := os.Open("secrets.txt")
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer file.Close()
+	var version int
+	var zoneid string
+	var recordid string
+	var email string
+	var authkey string
+	var recordType string
+	var domain string
+	fmt.Fscanf(file, "%d%s%s%s%s%s%s", &version, &zoneid, &recordid, &email, &authkey, &recordType, &domain)
+	//fmt.Println("valores", version, zoneid, recordid, email, authkey, recordType, domain)
+	cloudflare.UpdateAAAA(version, zoneid, recordid, email, authkey, recordType, domain, fmt.Sprint(glowing.GetIP()))
+	//fmt.Println(result)
 }
